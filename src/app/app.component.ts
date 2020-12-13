1ffe426ff427627315286b6fcf146eceb6c7668a
DanielCarrami <danielcarrami@gmail.com>
2020-12-13 12:14:37 -0600

    First commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
+import { DeviceDetectorService } from 'ngx-device-detector';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,8 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'dati';
+  
+  onResize(event) {
+    event.target.innerWidth;
+  }
 }
